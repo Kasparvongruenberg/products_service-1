0b88916047e6e27ffade581057890f1a0b82c0b3
Jeferson Moura <jefmmoreira@gmail.com>
2018-10-18 13:31:54 +0200

    Add missing health check endpoint

--- products_service/urls.py
+++ products_service/urls.py
@@ -47,7 +47,8 @@ urlpatterns = [
         schema_view.without_ui(cache_timeout=0), name='schema-json'),
     url(r'^api/docs/$', schema_view.with_ui('swagger', cache_timeout=0),
         name='schema-swagger-ui'),
-    url(r'^api/', include(router.urls))
+    url(r'^api/', include(router.urls)),
+    url(r'^health_check/', include('health_check.urls')),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
