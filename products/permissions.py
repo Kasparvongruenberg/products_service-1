9ed391dfd6387627fd9663c474e5b4a430edbf00
Greg Lind <gwlind@gmail.com>
2019-01-14 13:20:07 +0100

    default perm

--- products/permissions.py
+++ products/permissions.py
@@ -1,2 +1,14 @@
-# from rest_framework import permissions
-# create permissions
+from django.conf import settings
+from rest_framework.permissions import IsAuthenticated
+
+
+class AllowOptionsAuthentication(IsAuthenticated):
+    def has_permission(self, request, view):
+        if request.method == 'OPTIONS':
+            return True
+
+        if getattr(request, 'session', None) and \
+                request.session.get('jwt_iss') == settings.JWT_ALLOWED_ISSUER:
+            return True
+
+        return request.user and request.user.is_authenticated
