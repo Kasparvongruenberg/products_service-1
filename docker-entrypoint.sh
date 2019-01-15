020a9bbe97742cd55639b8339dac60b05c8b8c42
Ralf Kostulski <639859+ralfzen@users.noreply.github.com>
2019-01-15 16:25:29 +0100

    Add running migrations to entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# It is responsability of the deployment orchestration to execute before
-# migrations, create default admin user, populate minimal data, etc.
+python manage.py migrate
 
 gunicorn products_service.wsgi --config products_service/gunicorn_conf.py
